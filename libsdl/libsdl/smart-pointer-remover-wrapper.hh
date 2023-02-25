68169c423e61ca7b624ae3c750ff0db7c081abb8
Alex van Vliet <alex.van-vliet@epita.fr>
2023-02-25 21:07:39 +0100

    feat: add a smart pointer remover wrapper
    Note: the rvalue reference is supported and works as intended, it should
    not have been removed in the first place.

---
+++ libsdl/libsdl/smart-pointer-remover-wrapper.hh
@@ -0,0 +1,64 @@
+#ifndef LIBSDL_SMART_POINTER_REMOVER_WRAPPER_HH
+#define LIBSDL_SMART_POINTER_REMOVER_WRAPPER_HH
+
+#include <memory>
+#include "function-wrapper.hh"
+
+namespace libsdl {
+    namespace detail {
+        // Helper class to get a T* from a unique_ptr<T>, and itself from everything else
+        template<typename T>
+        class AsRaw {
+        public:
+            static constexpr bool implemented = false;
+        };
+
+        template<typename T, typename D>
+        class AsRaw<std::unique_ptr<T, D>> {
+        public:
+            static constexpr bool implemented = true;
+
+            using in_t = std::unique_ptr<T, D>;
+            using out_t = typename in_t::pointer;
+
+            static constexpr out_t call(in_t &t) {
+                return t.get();
+            }
+
+            static constexpr out_t call(const in_t &t) {
+                return t.get();
+            }
+
+            static constexpr out_t call(in_t &&t) {
+                return t.get();
+            }
+        };
+
+        auto as_raw(auto &&in) {
+            using as_raw_t = AsRaw<std::remove_cvref_t<decltype(in)>>;
+            if constexpr (as_raw_t::implemented) {
+                return AsRaw<std::remove_cvref_t<decltype(in)>>::call(std::forward<decltype(in)>(in));
+            } else {
+                return in;
+            }
+        }
+    }
+
+    /**
+     * Wrapper to remove the smart pointers in the arguments
+     *
+     * @tparam FUN The function to wrap
+     * @tparam OUT The new return type
+     */
+    template<FunctionWrapperConcept FUN>
+    class SmartPointerRemoverWrapper {
+    public:
+        using out_t = FUN::out_t;
+
+        constexpr out_t operator()(auto &&... arguments) const {
+            return FUN{}(detail::as_raw(std::forward<decltype(arguments)>(arguments))...);
+        }
+    };
+}
+
+#endif //LIBSDL_SMART_POINTER_REMOVER_WRAPPER_HH
---
+++ libsdl/tests/smart-pointer-remover-wrapper.cc
@@ -0,0 +1,45 @@
+#include <catch2/catch_all.hpp>
+#include <libsdl/smart-pointer-remover-wrapper.hh>
+
+using namespace libsdl;
+
+namespace {
+    int my_function(const int *value) {
+        return *value;
+    }
+
+    constexpr SmartPointerRemoverWrapper<FunctionWrapper<my_function>> my_wrapped_function;
+}
+
+TEST_CASE("smart pointer remover wrapped functions should accept unique pointers as arguments", "[libsdl]") {
+    REQUIRE(std::is_same_v<decltype(my_wrapped_function)::out_t, int>);
+
+    auto res = my_wrapped_function(std::make_unique<int>(2));
+
+    REQUIRE(res == 2);
+
+    auto ptr = std::make_unique<int>(3);
+    res = my_wrapped_function(ptr);
+
+    REQUIRE(res == 3);
+
+    ptr = std::make_unique<int>(4);
+    res = my_wrapped_function(std::move(ptr));
+
+    REQUIRE(res == 4);
+
+    const auto cptr = std::make_unique<int>(5);
+    res = my_wrapped_function(cptr);
+
+    REQUIRE(res == 5);
+}
+
+TEST_CASE("smart pointer remover wrapped functions should still accept raw pointers", "[libsdl]") {
+    REQUIRE(std::is_same_v<decltype(my_wrapped_function)::out_t, int>);
+
+    int value = 6;
+
+    auto res = my_wrapped_function(&value);
+
+    REQUIRE(res == 6);
+}
