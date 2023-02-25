2da4a3b3fe93b812255c39177aacbc9ecdf9f1b4
Alex van Vliet <alex.van-vliet@epita.fr>
2023-02-25 22:20:40 +0100

    refactor: replace wrapper by smaller wrappers and add tests

--- libsdl/libsdl/sdl.hh
+++ libsdl/libsdl/sdl.hh
@@ -5,28 +5,77 @@
 #include <memory>
 #include "function-wrapper.hh"
 #include "type-wrapper.hh"
-#include "wrapper.hh"
+#include "error-checker-wrapper.hh"
+#include "smart-pointer-remover-wrapper.hh"
 
 namespace {
     // Creates a deleter for unique_ptr from a function
     template<auto FUN>
     using Deleter = decltype([](auto *arg) { FUN(arg); });
     // Creates a deleter for unique_ptr which does nothing
-    using NoDeleter = decltype([](auto *arg) { });
+    using NoDeleter = decltype([](auto *arg) {});
+
+    // Create a functor to get the SDL error
+    using GetError = decltype([]() { return SDL_GetError(); });
+
+    template<auto FUN>
+    using UncheckedWrapper = libsdl::SmartPointerRemoverWrapper<libsdl::FunctionWrapper<FUN>>;
+
+    template<typename T, typename ERROR>
+    class Checker {
+    };
+
+    template<std::signed_integral T, typename ERROR>
+    class Checker<T, ERROR> {
+    public:
+        void operator()(auto res) {
+            if (res < 0) {
+                throw std::runtime_error(ERROR{}());
+            }
+        }
+    };
+
+    template<typename T, typename ERROR>
+    class Checker<T *, ERROR> {
+    public:
+        void operator()(const T *res) {
+            if (res == nullptr) {
+                throw std::runtime_error(ERROR{}());
+            }
+        }
+    };
+
+    template<auto FUN, typename OUT, typename ERROR>
+    class WrapperHelper {
+    public:
+        using type = libsdl::ErrorCheckerWrapper<UncheckedWrapper<FUN>, Checker<OUT, ERROR>>;
+    };
+
+    template<auto FUN, typename ERROR>
+    class WrapperHelper<FUN, void, ERROR> {
+    public:
+        using type = UncheckedWrapper<FUN>;
+    };
+
+    template<auto FUN, typename ERROR=GetError>
+    using Wrapper = typename WrapperHelper<FUN, typename libsdl::FunctionWrapper<FUN>::out_t, ERROR>::type;
+
+    template<auto FUN, typename OUT, typename ERROR=GetError>
+    using TypedWrapper = libsdl::TypeWrapper<Wrapper<FUN, ERROR>, OUT>;
 }
 
 namespace libsdl {
     // The SDL Wrappers
-    constexpr Wrapper<FunctionWrapper<SDL_Init>> init;
+    constexpr Wrapper<SDL_Init> init;
     using Window = std::unique_ptr<SDL_Window, Deleter<SDL_DestroyWindow>>;
-    constexpr TypeWrapper<Wrapper<FunctionWrapper<SDL_CreateWindow>, true>, Window> create_window;
+    constexpr TypedWrapper<SDL_CreateWindow, Window> create_window;
     using WindowSurface = std::unique_ptr<SDL_Surface, NoDeleter>;
-    constexpr TypeWrapper<Wrapper<FunctionWrapper<SDL_GetWindowSurface>>, WindowSurface> get_window_surface;
-    constexpr Wrapper<FunctionWrapper<SDL_FillRect>> fill_rect;
-    constexpr Wrapper<FunctionWrapper<SDL_MapRGB>, false> map_rgb;
-    constexpr Wrapper<FunctionWrapper<SDL_UpdateWindowSurface>> update_window_surface;
-    constexpr Wrapper<FunctionWrapper<SDL_PollEvent>, false> poll_event;
-    constexpr Wrapper<FunctionWrapper<SDL_Quit>> quit;
+    constexpr TypedWrapper<SDL_GetWindowSurface, WindowSurface> get_window_surface;
+    constexpr Wrapper<SDL_FillRect> fill_rect;
+    constexpr UncheckedWrapper<SDL_MapRGB> map_rgb;
+    constexpr Wrapper<SDL_UpdateWindowSurface> update_window_surface;
+    constexpr UncheckedWrapper<SDL_PollEvent> poll_event;
+    constexpr Wrapper<SDL_Quit> quit;
 
     // Just a simple Context class to use RAII for init and quit
     class Context {
