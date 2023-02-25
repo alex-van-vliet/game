0a9b866942ee828e6e3d19f2013a0efa25ff3d15
Alex van Vliet <alex.van-vliet@epita.fr>
2023-02-25 19:24:31 +0100

    feat: add a wrapper that only does the error checking

---
+++ libsdl/libsdl/error-checker-wrapper.hh
@@ -0,0 +1,36 @@
+#ifndef LIBSDL_ERROR_CHECKER_WRAPPER_HH
+#define LIBSDL_ERROR_CHECKER_WRAPPER_HH
+
+#include <utility>
+#include "function-wrapper.hh"
+
+namespace libsdl {
+    template<typename T, typename VALUE_TYPE>
+    concept ErrorChecker = requires(T t, VALUE_TYPE v){
+        t(v);
+    };
+
+
+    /**
+     * Wrapper to check if the function returns an error
+     *
+     * @tparam FUN The function to wrap
+     * @tparam CHECKER The function that checks the value
+     */
+    template<FunctionWrapperConcept FUN, ErrorChecker<typename FUN::out_t> CHECKER>
+    requires (!std::is_same_v<typename FUN::out_t, void>)
+    class ErrorCheckerWrapper {
+    public:
+        using out_t = FUN::out_t;
+
+        constexpr out_t operator()(auto &&... arguments) const {
+            auto res = FUN{}(std::forward<decltype(arguments)>(arguments)...);
+
+            CHECKER{}(res);
+
+            return res;
+        }
+    };
+}
+
+#endif //LIBSDL_ERROR_CHECKER_WRAPPER_HH
---
+++ libsdl/tests/error-checker-wrapper.cc
@@ -0,0 +1,34 @@
+#include <catch2/catch_all.hpp>
+#include <libsdl/error-checker-wrapper.hh>
+
+using namespace libsdl;
+
+namespace {
+    int my_function(int value) {
+        return value;
+    }
+
+    constexpr ErrorCheckerWrapper<FunctionWrapper<my_function>, decltype([](int value) {
+        if (value < 0)
+            throw std::runtime_error("Value should be non-negative.");
+    })> my_wrapped_function;
+
+    constexpr ErrorCheckerWrapper<FunctionWrapper<my_function>, decltype([](const int& value) {
+        if (value < 0)
+            throw std::runtime_error("Value should be non-negative.");
+    })> my_wrapped_function2;
+}
+
+TEST_CASE("error checker wrapped functions should have the return value checked", "[libsdl]") {
+    REQUIRE(std::is_same_v<decltype(my_wrapped_function)::out_t, int>);
+
+    auto res = my_wrapped_function(2);
+
+    REQUIRE(res == 2);
+
+    REQUIRE_THROWS_MATCHES(my_wrapped_function(-1), std::runtime_error,
+                           Catch::Matchers::Message("Value should be non-negative."));
+
+    REQUIRE_THROWS_MATCHES(my_wrapped_function2(-2), std::runtime_error,
+                           Catch::Matchers::Message("Value should be non-negative."));
+}
